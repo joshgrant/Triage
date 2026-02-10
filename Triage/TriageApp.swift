cb16d4cce703e1073328704ca2b5413112e4d4bf
Joshua Grant <joshgrant@pm.me>
2026-02-10 11:50:48 -0600

    Initial Commit

---
+++ Triage/TriageApp.swift
@@ -0,0 +1,32 @@
+//
+//  TriageApp.swift
+//  Triage
+//
+//  Created by Joshua Grant on 2/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TriageApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
